15c074a413f2ae45e98f80468a9b6d1f655d4539
gabrielrih <usuario@MacBook-Air-de-Usuario.local>
2024-07-12 14:29:20 -0300

    test: add unit tests for github-api ms

--- microservices/github-api/test/unit/test_queue.py
+++ microservices/github-api/test/unit/test_queue.py
@@ -1,8 +1,36 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from src.queue import QueueManager
 
 
 class TestQueueManager(TestCase):
-    def test_send_message(self):
-        pass
+    def setUp(self):
+        self.mock_repo_url = mock.patch('src.queue.envs.FORK_REPO_QUEUE_URL', return_value = 'fake_queue_url')
+        self.start_mocks()
+
+    def start_mocks(self):
+        self.mock_repo_url.start()
+
+    def tearDown(self):
+        self.stop_mocks()
+
+    def stop_mocks(self):
+        self.mock_repo_url.stop()
+
+    @mock.patch('boto3.client')
+    def test_send_message(self, mock_boto3_client):
+        # Given
+        mock_boto3_client.return_value = MockBoto3SQSClient()
+
+        # When
+        queue = QueueManager()
+        response = queue.send_message(data = 'fake_data')
+
+        # Then
+        mock_boto3_client.assert_called_once()
+        self.assertIsNone(response)
+
+
+class MockBoto3SQSClient:
+    def send_message(*_, **__) -> None:
+        return None
