48b926b1e3065302e0efa0432792a6359d703323
gabrielrih <usuario@MacBook-Air-de-Usuario.local>
2024-07-13 16:00:38 -0300

    fix: apply again github-api ms

--- tf/dev/main.tf
+++ tf/dev/main.tf
@@ -7,18 +7,18 @@ module "common" {
     aws_sqs_queue_name = var.common_aws_sqs_queue_name
 }
 
-# module "github-api" {
-#     source = "../modules/github-api"
-#     depends_on = [ module.common ]
-#     github_api_version = var.github_api_version
-#     aws_region = var.aws_region
-#     aws_default_vpc_id = module.common.default_vpc_id
-#     aws_default_subnet_a_id = module.common.default_subnet_a_id
-#     aws_default_subnet_b_id = module.common.default_subnet_b_id
-#     aws_ecs_cluster_id = module.common.ecs_cluster_id
-#     aws_sqs_queue_id = module.common.sqs_queue_id
-#     aws_sqs_queue_arn = module.common.sqs_queue_arn
-# }
+module "github-api" {
+    source = "../modules/github-api"
+    depends_on = [ module.common ]
+    github_api_version = var.github_api_version
+    aws_region = var.aws_region
+    aws_default_vpc_id = module.common.default_vpc_id
+    aws_default_subnet_a_id = module.common.default_subnet_a_id
+    aws_default_subnet_b_id = module.common.default_subnet_b_id
+    aws_ecs_cluster_id = module.common.ecs_cluster_id
+    aws_sqs_queue_id = module.common.sqs_queue_id
+    aws_sqs_queue_arn = module.common.sqs_queue_arn
+}
 
 module "github-clone" {
     count = var.is_to_deploy_github_clone_as_lambda == false ? 1 : 0
